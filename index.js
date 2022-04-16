86d84e9691835a04f4b35f50c9ae4b662d33c834
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-16 23:17:42 +0300

    Seen

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+    let diff = target - array[i];
+     for (let j = i + 1; j < array.length; j++){
+       if(array[j] === diff){
+        return true
+       }       
+      }      
+     }
+     return false
 }
 
 /* 
